547493bc2e8a9a3f20e362006b510050c2d5ce10
Farida <faridah.absi@gmail.com>
2018-10-28 21:46:37 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,38 @@
-function preventRefreshOnSubmit(){
-    document.querySelector('form').addEventListener('submit', function(event){
-      event.preventDefault()
-    })
+function retrieveEmployeeInformation(){
+
+return  document.querySelector('input').value;
+
 }
 
-preventRefreshOnSubmit()
+function addNewElementAsLi(fn){
+var A =  retrieveEmployeeInformation();
+
+var B =document.querySelector('.employee-list');
+
+var element = document.createElement('li');
+  element.innerHTML = A ;
+
+B.appendChild(element);
+}
+
+function addNewLiOnClick(){
+  var A =  document.querySelector('input').value;
+
+  var C =document.querySelector('.employee-list');
+
+var element = document.createElement('li');
+  element.innerHTML = A ;
+
+  C.appendChild(element);
+document.querySelector('input').value = "";
+}
+
+function clearEmployeeListOnLinkClick(){
+  let list = document.querySelector('.employee-list')
+    let button = document.querySelector('a');
+
+    button.addEventListener('click',function(){
+     list.innerHTML="";
+
+    })
+  }
